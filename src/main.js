a84003f7544b4d792997039f2d12d7e57c0c4d82
Nicolas Richard <nicolas_richard77@hotmail.com>
2018-08-28 15:47:02 -0400

    Removed unnecessary imports

--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import ImageCard from './components/ImageCard'
 import router from './router'
 
-import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
@@ -13,6 +11,6 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 new Vue({
   el: '#app',
   router,
-  components: {App, ImageCard},
+  components: {App},
   template: '<App/>'
-}).use(BootstrapVue)
+})
