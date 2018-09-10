4e112bd0f550bf64142c5d31699142656104d8f5
Nicolas Richard <nicolas_richard77@hotmail.com>
2018-09-10 10:15:05 -0400

    node server API for searching GIFs with GIPHY

---
+++ vue-giphy-api/app.js
@@ -0,0 +1,16 @@
+let express = require('express')
+let logger = require('morgan')
+require('dotenv').load()
+
+let indexRouter = require('./routes/index')
+let usersRouter = require('./routes/users')
+
+let app = express()
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/', indexRouter)
+
+module.exports = app
---
+++ vue-giphy-api/routes/index.js
@@ -0,0 +1,37 @@
+let express = require('express')
+let request = require('request')
+let querystring = require('querystring')
+
+let router = express.Router()
+
+/**
+ * Generates an URL for GIPHY api using parameters provided
+ * @param target Target page
+ * @param parameters GET parameters to send
+ * @returns URL for GIPHY api
+ */
+function generateUrl(target, parameters) {
+  let generalOptions = {
+    api_key: process.env.GIPHY_API_KEY,
+    limit: 5,
+    fmt: 'json'
+  }
+  Object.assign(parameters, generalOptions)
+
+  return 'http://api.giphy.com/v1/gifs/' + target + '?' + querystring.stringify(parameters)
+}
+
+
+router.get('/', function (req, res) {
+  request(generateUrl('trending', {})).pipe(res)
+})
+
+router.get('/search', function (req, res) {
+  let parameters= {
+    q: req.query.q,
+  }
+
+  request(generateUrl('search', parameters)).pipe(res)
+})
+
+module.exports = router
