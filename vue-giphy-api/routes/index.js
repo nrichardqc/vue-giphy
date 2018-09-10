36c575f042bd969a9f432a20eb53de46200c233d
Nicolas Richard <nicolas_richard77@hotmail.com>
2018-09-10 10:44:31 -0400

    Lint + comments

--- vue-giphy-api/routes/index.js
+++ vue-giphy-api/routes/index.js
@@ -10,7 +10,7 @@ let router = express.Router()
  * @param parameters GET parameters to send
  * @returns URL for GIPHY api
  */
-function generateUrl(target, parameters) {
+function generateUrl (target, parameters) {
   let generalOptions = {
     api_key: process.env.GIPHY_API_KEY,
     limit: 5,
@@ -21,14 +21,16 @@ function generateUrl(target, parameters) {
   return 'http://api.giphy.com/v1/gifs/' + target + '?' + querystring.stringify(parameters)
 }
 
+// Note : it would have been easier to use node-http-proxy or something similar,
+// but I wanted to test some simple Node.js functions.
 
 router.get('/', function (req, res) {
   request(generateUrl('trending', {})).pipe(res)
 })
 
 router.get('/search', function (req, res) {
-  let parameters= {
-    q: req.query.q,
+  let parameters = {
+    q: req.query.q
   }
 
   request(generateUrl('search', parameters)).pipe(res)
